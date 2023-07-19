d859b33db4c6a553a5340e8555e97bc988855a93
Eric Jutrzenka <e.jutrzenka@icloud.com>
2023-07-19 15:53:42 -0700

    initial commit

---
+++ tilemap_src/01_tiled/layer_3.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="layer_3" tilewidth="8" tileheight="8" tilecount="49" columns="1">
+ <image source="layer_3_tiles.png" trans="00ffff" width="8" height="392"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="32" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="36" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="48" wangid="0,0,0,0,1,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
