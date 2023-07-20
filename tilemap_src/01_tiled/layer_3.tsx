eee4fea7ce21ae72df419e4c3c342e67a0e2d8d0
Eric Jutrzenka <e.jutrzenka@icloud.com>
2023-07-19 22:36:29 -0700

    wip\

--- tilemap_src/01_tiled/layer_3.tsx
+++ tilemap_src/01_tiled/layer_3.tsx
@@ -1,14 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="layer_3" tilewidth="8" tileheight="8" tilecount="49" columns="1">
+ <editorsettings>
+  <export target="../../layer_3.bin" format="next"/>
+ </editorsettings>
  <image source="layer_3_tiles.png" trans="00ffff" width="8" height="392"/>
- <wangsets>
-  <wangset name="Unnamed Set" type="edge" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-   <wangtile tileid="6" wangid="0,0,0,0,1,0,0,0"/>
-   <wangtile tileid="7" wangid="0,0,1,0,0,0,0,0"/>
-   <wangtile tileid="32" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="36" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="48" wangid="0,0,0,0,1,0,0,0"/>
-  </wangset>
- </wangsets>
 </tileset>
